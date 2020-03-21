23618bc9578232131a84307577317b7816f2b195
maanlamp <32102935+maanlamp@users.noreply.github.com>
2020-03-21 17:25:48 +0100

    Update Just.js

--- Just.js
+++ Just.js
@@ -3,16 +3,21 @@
 import JustHTTPS from "./JustHTTPS.js";
 import JustDOM from "./JustDOM.js";
 
-class Just {
+export default class Just {
 
+	// TODO: Do we want to support rendering templates through Just.js?
+	//	If so: do we put it in DOM or directly in Just?
+	//	If not: why is this here?
 	static render (template) {
 		// TODO: Implement
 		throw `Not implemented.`;
 	}
 
 	static dom = JustDOM;
+
+	// TODO: Decide wether we only support HTTPS, or support HTTP too,
+	//	and if so: do we copy HTTPS or do we allow all other protocols?
+	//	Other protocols will probably each require a different class.
 	static https = JustHTTPS;
 
 }
-
-export default Just;
